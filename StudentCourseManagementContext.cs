be6cfd9322bec6bd5d8a7ceb0110d5c177a1638f
raghadshaar <raghadshaar8@gmail.com>
2026-08-02 00:16:53 +0300

    feat: configure StudentCourseManagementContext

---
+++ StudentCourseManagementContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentCourseManagementSystem
+{
+    public class StudentCourseManagementContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;
+                                        Initial Catalog=StudentCourseManagement;
+                                        Integrated Security=True;");
+
+        }
+        
+
+        
+    }
+}
