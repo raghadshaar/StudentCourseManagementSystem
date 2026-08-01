87db90ca5dc1e30899d01d556bc5b38fb3ccecbf
raghadshaar <raghadshaar8@gmail.com>
2026-08-01 23:17:02 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentCourseManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
